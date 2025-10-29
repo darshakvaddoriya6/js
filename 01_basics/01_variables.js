2e9ede5b8876294c43e58c6259aa7781171d5149
Darshak Vaddoriya <167399983+darshakvaddoriya6@users.noreply.github.com>
2025-10-29 05:28:07 +0000

    variables  done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234
+let accountEmail = "one@gmail.com"
+var accountPassword = "1234"
+accountCity = "ahmedbad"
+
+// accountId =  1111 // not allowed
+
+accountEmail= "hey@hey.com"
+accountPassword= "4321"
+accountCity= "Mumbai"
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
