2b35d7c7400adcc7a0277e020305e6424bb56998
Darshak Vaddoriya <167399983+darshakvaddoriya6@users.noreply.github.com>
2025-10-29 07:10:30 +0000

    conversionoprations and comparisions done

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -27,7 +27,51 @@ let bolleanIsLoggedIn = Boolean(loggedIn)
 let someNumber = 33 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
 // 33 => string
+
+//********************* Oprations *********************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 / 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+let str1 = "hello"
+let str2 = " darshak"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log( 2 + 2 + "1");
+// console.log(Number("1") + 2);
+
+// console.log((2 + 3) * 4 % 3);
+
+// console.log(+true);
+
+let  num1 , num2 , num3;
+
+num1 = num2 = num3 = 2 + 2
+// console.log(num1);
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+
+
+
 
