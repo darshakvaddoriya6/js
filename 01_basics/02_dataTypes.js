9cf2b79c63297794a46cd0fc9d85e6c31c4e686c
Darshak Vaddoriya <167399983+darshakvaddoriya6@users.noreply.github.com>
2025-10-29 05:45:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // terat all js code as a newer version
+
+// alert("hellow") // we are using nodejs, not browser
+
+
+let name = "darshak"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// number = > 2 to power 53
+// Bigint
+// String => ""
+// boolean => true/false
+// null => stanloane value
+// undefined =>
+// symbol => unique    
+
+
+// object =>
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
