64a5b92870c3a3b61e062abbaa0cc4df931196ff
Darshak Vaddoriya <167399983+darshakvaddoriya6@users.noreply.github.com>
2025-10-30 04:26:16 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,48 @@
+
+if (true) {
+    let a = 10
+    const c = 40
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "darshak" 
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+    
+}
+
+
+if (true) {
+    const username = "darshak"
+    if (username === "darshak") {
+        const website = "youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+function addOne(num) {
+    return num + 1
+}
+
+// console.log(addOne(5));
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5));
